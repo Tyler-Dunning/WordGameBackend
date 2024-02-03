130a85d0b98b8325e62d25e41d9999f12e84bacc
Tyler-Dunning <118504218+Tyler-Dunning@users.noreply.github.com>
2024-02-03 12:26:24 -0800

    remove cors

--- api/index.py
+++ api/index.py
@@ -1,11 +1,8 @@
 from flask import Flask,request
-from flask_cors import CORS, cross_origin
 import csv
 
 app = Flask(__name__)
-cors = CORS(app)
 
-app.config['CORS_HEADERS'] = 'Content-Type'
 
 # Specify the path to your CSV file and the column name containing the strings
 csv_file_path = 'words_altered.csv'
@@ -20,7 +17,6 @@ with open(csv_file_path, 'r', newline='', encoding='utf-8') as csvfile:
         csv_strings_set.add(string_from_csv)
 
 @app.route("/anagrams")
-@cross_origin()
 def anagrams():
     
     query = request.args.get('inputs')
@@ -51,7 +47,6 @@ def anagrams():
     return result
 
 @app.route("/wordhunt")
-@cross_origin()
 def wordhunt():
     
     query = request.args.get('inputs')
